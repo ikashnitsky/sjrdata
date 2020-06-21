0c6f17f9c2d16c5c2fa5623690bb433c2e0cfe23
Rob Hyndman <robjhyndman@gmail.com>
2020-06-21 15:24:34 +1000

    Updated to 2019 data

--- R/data.R
+++ R/data.R
@@ -2,30 +2,32 @@
 
 #' SCImago Journal Rank
 #'
-#' SCImago Journal Rank for all jounals indexed by Scopus in 1999--2018
+#' SCImago Journal Rank for all journals indexed by Scopus in 1999--2019
 #'
 #' @format
-#'   A tibble with 541877 rows and 20 variables:
+#'   A tibble with 582,889 rows and 21 variables:
 #'   \describe{
 #'     \item{year}{Year of SCImago Journal Ranking calculation.}
 #'     \item{rank}{Rank of the journal among all journals.}
 #'     \item{sourceid}{Database ID of the journal.}
-#'     \item{title}{Jounal's title.}
+#'     \item{title}{Journal's title.}
 #'     \item{type}{Type: "journal", "book series", "trade journal", or "conference and proceedings"}
 #'     \item{issn}{ISSN journal identifier.}
 #'     \item{sjr}{SCImago Journal Rank indicator. It expresses the average number of weighted citations received in the selected year by the documents published in the selected journal in the three previous years, --i.e. weighted citations received in year X to documents published in the journal in years X-1, X-2 and X-3. See [detailed description of SJR](https://www.scimagojr.com/SCImagoJournalRank.pdf) (PDF).}
 #'     \item{sjr_best_quartile}{Highest quartile of the journal among all categories it belongs to.}
-#'     \item{h_index}{Hirsch index of the journal. The h index expresses the journal's number of articles (h) that have received at least h citations. It quantifies both journal scientific productivity and scientific impact and it is also applicable to scientists, countries, etc. ([see H-index wikipedia definition](http://en.wikipedia.org/wiki/Hirsch_number)).}
+#'     \item{h_index}{Hirsch index of the journal. The h index expresses the journal's number of articles (h) that have received at least h citations. It quantifies both journal scientific productivity and scientific impact and it is also applicable to scientists, countries, etc. ([see H-index Wikipedia definition](http://en.wikipedia.org/wiki/Hirsch_number)).}
 #'     \item{total_docs_year}{Total number of published documents within a specific year. All types of documents are considered, including citable and non citable documents.}
 #'     \item{total_docs_3years}{Published documents in the three previous years (selected year documents are excluded), i.e.when the year X is selected, then X-1, X-2 and X-3 published documents are retrieved. All types of documents are considered, including citable and non citable documents.}
 #'     \item{total_refs}{Total number of citations received by a journal to the documents published within a specific year.}
-#'     \item{total_cites_3years}{Number of citations received in the seleted year by a journal to the documents published in the three previous years, --i.e. citations received in year X to documents published in years X-1, X-2 and X-3. All types of documents are considered.}
+#'     \item{total_cites_3years}{Number of citations received in the selected year by a journal to the documents published in the three previous years, --i.e. citations received in year X to documents published in years X-1, X-2 and X-3. All types of documents are considered.}
 #'     \item{citable_docs_3years}{Number of citable documents published by a journal in the three previous years (selected year documents are excluded). Exclusively articles, reviews and conference papers are considered..}
 #'     \item{cites_doc_2years}{Average citations per document in a 2 year period. It is computed considering the number of citations received by a journal in the current year to the documents published in the two previous years, --i.e. citations received in year X to documents published in years X-1 and X-2. Comparable to Journal Impact Factor.}
 #'     \item{ref_doc}{Average number of references per document in the selected year..}
 #'     \item{country}{Country of the publisher.}
+#'     \item{region}{Region of the publisher.}
 #'     \item{publisher}{Publisher of the journal.}
-#'     \item{categories}{Categories the jounal belongs to.}
+#'     \item{coverage}{Years covered.}
+#'     \item{categories}{Categories the journal belongs to.}
 #'   }
 #'
 #' @source
@@ -67,14 +69,15 @@
 
 #' SCImago Country Rank
 #'
-#' SCImago Country Rank for all papers indexed by Scopus in 1996--2018. Calculations year-by-year.
+#' SCImago Country Rank for all papers indexed by Scopus in 1996--2019. Calculations year-by-year.
 #'
 #' @format
-#'   A tibble with 5146 rows and 9 variables:
+#'   A tibble with 5395 rows and 10 variables:
 #'   \describe{
 #'     \item{year}{Year of SCImago Country Ranking calculation.}
 #'     \item{rank}{Rank of the country in a given year.}
-#'     \item{country}{Country.}
+#'     \item{country}{Name of country.}
+#'     \item{region}{Region of the world.}
 #'     \item{documents}{Number of documents published during the selected year. It is usually called the country's scientific output.}
 #'     \item{citable_documents}{Selected year citable documents. Exclusively articles, reviews and conference papers are considered.}
 #'     \item{citations}{Number of citations by the documents published during the source year, --i.e. citations in years X, X+1, X+2, X+3... to documents published during year X. When referred to the period 1996-2017, all published documents during this period are considered.}
@@ -91,13 +94,14 @@
 
 #' SCImago Country Rank
 #'
-#' SCImago Country Rank for all papers indexed by Scopus in 1996--2018. Calculations for the whole period.
+#' SCImago Country Rank for all papers indexed by Scopus in 1996--2019. Calculations for the whole period.
 #'
 #' @format
-#'   A tibble with 239 rows and 8 variables:
+#'   A tibble with 240 rows and 9 variables:
 #'   \describe{
 #'     \item{rank}{Rank of the country in a given year.}
-#'     \item{country}{Country.}
+#'     \item{country}{Name of country.}
+#'     \item{region}{Region of the world.}
 #'     \item{documents}{Number of documents published during the selected year. It is usually called the country's scientific output.}
 #'     \item{citable_documents}{Selected year citable documents. Exclusively articles, reviews and conference papers are considered.}
 #'     \item{citations}{Number of citations by the documents published during the source year, --i.e. citations in years X, X+1, X+2, X+3... to documents published during year X. When referred to the period 1996-2017, all published documents during this period are considered.}
